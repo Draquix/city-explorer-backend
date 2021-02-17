1beebc545f5d0bb479213dc16ec5c2b92c5649ab
Draquix <Draquix@yahoo.com>
2021-02-17 15:55:11 -0600

    meeting requirements of lab06

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # city-explorer-backend
 revamp attempt at server backend for labs 6-10
+# Lab-06
+**Author:** Daniel Rogahn
+**Version:** 1.0.0
+
+## Overview
+This is the first steps to building the City Explorer App in which we write a server
+that runs in node.js and deploys to heroku.  It processes requests in the form of GPS
+and finds information about the location in the form of objects.
+
+## Getting Started
+As per the trello.com cards, found by copying the existing board: City Explorer...
+1. Repository set up- making the various files needed for the app and deploying to heroku
+2. Locations- write code in the server to handle requests for the name of a location that will return data
+3. Weather - write server code that will handle a request for the typical weather conditions to be expected
+*the json files with the objects that are returned as results to the server requests were provided in the starter code*
+
+## Architecture
+This uses node.js from a basic npm init that utilizes dotenv, cors, and express.
+It is deployed on heroku but also has a standard GitHub repository.  
+
+02-15-2021 2:00 - Application has basic scaffolding and is about to be deployed to heroku.
+
+## Credits and Collaborations
+Special thanks to Yvette Clapp and Ben Hemann for being present and working through the steps as we all try to get through this project in Remo.
---
+++ server.js
@@ -0,0 +1,58 @@
+'use strict';
+console.log('server.js is running');
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const superagent = require('superagent');
+const { response } = require('express');
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`));
+
+app.get('/', (req,res) => {
+    res.send(`Connected on Port: ${PORT}`);
+});
+
+function Location(searchedCity, display_name, latitude, longitude) {
+    this.searchedCity = searchedCity;
+    this.formatted_query = display_name;
+    this.latitude = parseFloat(latitude);
+    this.longitude = parseFloat(longitude);
+}
+
+function Weather(weather,valid_date) {
+    this.weather = weather;
+    this.valid_date = valid_date;
+}
+
+app.get('/location', (req,res) => {
+    const dataArrayFromJson = require('./data/location.json');
+    const dataObjectFromJson = dataArrayFromJson[0];
+
+    const searchedCity = req.query.city;
+    const newLocation = new Location(searchedCity,dataObjectFromJson.display_name,dataObjectFromJson.lat,dataObjectFromJson.lon);
+
+    res.send(newLocation);
+});
+
+app.get('/weather', (req,res) => {
+    const dataArrayFromJson = require('./data/weather.json');
+    const dataObjectFromJson = dataArrayFromJson.data[0];
+    
+    const newWeather = new Weather(dataObjectFromJson.weather.description,dataObjectFromJson.valid_date);
+
+    res.send(newWeather);
+});
+
+app.use('*', (req,res) => {
+    res.status(500).send('Status: 500<br> Server has error processing query.');
+});
+
+
+// .catch(error => {
+//     res.status(500).send('location query failed');
+// });
